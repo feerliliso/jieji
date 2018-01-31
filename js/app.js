d3cc160c8fc268510a7860ec03c5a8d0ceb1d535
feerliliso <feerliliso@hotmail.com>
2018-01-31 21:41:57 +0800

    add new code
    rt

--- js/app.js
+++ js/app.js
@@ -27,6 +27,7 @@ Enemy.prototype.render = function() {
 // 把所有敌人的对象都放进一个叫 allEnemies 的数组里面
 // 把玩家对象放进一个叫 player 的变量里面
 
+    player = new Player(1,2);
 
 // 这段代码监听游戏玩家的键盘点击事件并且代表将按键的关键数字送到 Play.handleInput()
 // 方法里面。你不需要再更改这段代码了。
