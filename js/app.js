39c000b43a4c2d20e346685515c165afd83f872a
feerliliso <feerliliso@hotmail.com>
2018-01-31 21:47:45 +0800

    add some commit
    add some commit

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 var Enemy = function(x,y) {
     // 要应用到每个敌人的实例的变量写在这里
     // 我们已经提供了一个来帮助你实现更多
+this.x= x;
+this.y=y;
     // 敌人的图片或者雪碧图，用一个我们提供的工具函数来轻松的加载文件
     this.sprite = 'images/enemy-bug.png';
 };
@@ -11,16 +13,44 @@ var Enemy = function(x,y) {
 Enemy.prototype.update = function(dt) {
     // 你应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
     // 都是以同样的速度运行的
+    //this.x += dt * this.speed
 };
 
 // 此为游戏必须的函数，用来在屏幕上画出敌人，
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-
+Enemy.prototype.checkCollision = function (player) {
+   if (this.y === player.y) {
+      console.log(`collision happened!! enemy.x: ${this.x}, player.x: ${player.x}`);
+  } else {
+       console.log(`player's safe!! enemy.x: ${this.x}, player.x: ${player.x}`);
+   }
+ }
 // 现在实现你自己的玩家类
 // 这个类需要一个 update() 函数， render() 函数和一个 handleInput()函数
+var allEnemies = [new Enemy()]
+//var player = new Player()
+var Player = function (x,y) {
+  this.x=x;
+  this.y=y;
+  this.sprite = "images/char-boy.png";
+};
+Player.prototype.update = function (dt) {
 
+};
+Player.prototype.handleInput = function (movement) {
+switch (movement) {
+  case 'left':this.x-=101;break;
+  case 'right':this.x+=101;break;
+  case 'up':this.y-=83;break;
+  case 'down':this.y+=83;break;
+
+  }
+};
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y)
+};
 
 // 现在实例化你的所有对象
 // 把所有敌人的对象都放进一个叫 allEnemies 的数组里面
